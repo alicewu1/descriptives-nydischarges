5d002ea6a8ad72f610f1f3229527a4022fade9f1
Alice Wu <110137320+alicewu1@users.noreply.github.com>
2022-09-20 15:55:10 -0400

    Updates3

--- tableone.py
+++ tableone.py
@@ -1,15 +1,16 @@
 import pandas as pd 
 from tableone import TableOne, load_dataset ## Receiving ImportError: tableone cannot be imported from partially initialized module 'tableone' (most likely due to a circular import)
 
-sparcs=pd.read_csv('https://health.data.ny.gov/resource/gnzp-ekau.csv')
+sparcs=pd.read_csv('https://health.data.ny.gov/resource/gnzp-ekau.csv') # used csv link from API
 sparcs
 sparcs.dtypes
-sparcs_columns = ['gender', 'length_of_stay', 'emergency_department_indicator']
+sparcs_columns = ['gender', 'length_of_stay', 'total_costs']
 sparcs_categorical = ['gender', 'length_of_stay']
 sparcs_groupby = ['length_of_stay']
-sparcs_labels={'emergency_department_indicator': 'ED_indicator'}
+sparcs_labels={'total_costs': 'final_costs'}
 
 sparcsTab1 = TableOne(sparcs, columns=sparcs_columns, categorical=sparcs_categorical, groupby=sparcs_groupby, rename=sparcs_labels, pval=False )
 ## still receiving a 'TableOne' is not defined error due to import error
+## used Google Colab to resolve errors
 
 print(sparcsTab1.to_csv('data/tableone_test.csv'))
